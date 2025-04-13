d0ce2b01dde36725de1f4e2180ac8826d1ca3824
CenaAlex <148414673+CenaAlex@users.noreply.github.com>
2025-04-13 16:55:02 +0800

    Update db.js

--- _helpers/db.js
+++ _helpers/db.js
@@ -2,7 +2,7 @@ const config =require('config.json');
 const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
-modul.exports = db = {};
+module.exports = db = {};
 
 initialize();
 
